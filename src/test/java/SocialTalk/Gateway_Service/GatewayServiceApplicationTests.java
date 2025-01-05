db6718954f0720e4dfa618973b96e2f6ac2e88c1
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2025-01-05 20:08:29 +0100

    Create gateway-service

---
+++ src/test/java/SocialTalk/Gateway_Service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SocialTalk.Gateway_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
